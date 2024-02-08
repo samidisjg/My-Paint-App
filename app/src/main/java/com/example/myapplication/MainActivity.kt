c82cd16b92033b30a38610fc75cb2cf7aca9dbd6
samidisjg <samidisjg@gmail.com>
2024-02-09 01:04:43 +0530

    completion of the paint app. Done

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.Toast
+import com.example.myapplication.PaintView.Companion.colorList
+import com.example.myapplication.PaintView.Companion.currentBrush
+import com.example.myapplication.PaintView.Companion.pathList
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
 
         val redBtn = findViewById<ImageButton>(R.id.redColor)
         val blueBtn = findViewById<ImageButton>(R.id.blueColor)
@@ -27,19 +32,36 @@ class MainActivity : AppCompatActivity() {
 
         redBtn.setOnClickListener{
             Toast.makeText(this,"Clicked", Toast.LENGTH_SHORT).show()
+            paintBrush.color = Color.RED
+            currentColor(paintBrush.color)
         }
         blueBtn.setOnClickListener {
             Toast.makeText(this,"Clicked", Toast.LENGTH_SHORT).show()
+            paintBrush.color = Color.BLUE
+            currentColor(paintBrush.color)
         }
         blackBtn.setOnClickListener {
             Toast.makeText(this,"Clicked", Toast.LENGTH_SHORT).show()
+            paintBrush.color = Color.BLACK
+            currentColor(paintBrush.color)
         }
         eraser.setOnClickListener {
             Toast.makeText(this,"Clicked", Toast.LENGTH_SHORT).show()
+            pathList.clear()
+            colorList.clear()
+            path.reset()
+
         }
         magentaBtn.setOnClickListener {
             Toast.makeText(this,"Clicked", Toast.LENGTH_SHORT).show()
+            paintBrush.color = Color.MAGENTA
+            currentColor(paintBrush.color)
         }
 
+    }
+
+    private fun currentColor(color: Int){
+        currentBrush = color
+        path = Path()
     }
 }
